3a8aec91334a327e78d82898c4ad2b7b36cd683b
Gary Shirk <gary@mfluent.com>
2015-01-25 16:13:14 -0600

    model complete

--- FlagQuiz/FlagQuiz/Model.swift
+++ FlagQuiz/FlagQuiz/Model.swift
@@ -3,4 +3,113 @@
 //
 import Foundation
 
+protocol ModelDelegate {
+    func settingsChanged()
+}
 
+class Model {
+
+    // keys for storing date in NSUserDefaults
+    private let regionsKey = "FlagQuizKeyRegions"
+    private let guessesKey = "FlagQuizGuesses"
+    
+    // delegate reference
+    private var delegate: ModelDelegate! = nil
+    
+    var numberOfGuesses = 4
+    
+    private var enabledRegions = [
+        "Africa" : false,
+        "Asia" : false,
+        "Europe" : false,
+        "North_America" : false,
+        "Oceania" : false,
+        "South_America" : false
+    ]
+    
+    let numberOfQuestions = 10
+    private var allCountries: [String] = []
+    private var countriesInEnabledRegions: [String] = []
+    
+    init(delegate: ModelDelegate, numberOfQuestions: Int) {
+        
+        self.delegate = delegate
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        
+        let tempGuesses = userDefaults.integerForKey(guessesKey)
+        if tempGuesses != 0 {
+            numberOfGuesses = tempGuesses
+        }
+        
+        if let tempRegions = userDefaults.dictionaryForKey(regionsKey) {
+            self.enabledRegions = tempRegions as [String : Bool]
+        }
+        
+        let paths = NSBundle.mainBundle().pathsForResourcesOfType("png", inDirectory: nil) as [String]
+        for path in paths {
+            if !path.lastPathComponent.hasPrefix("AppIcon") {
+                allCountries.append(path.lastPathComponent)
+            }
+        }
+        
+        regionsChanged()
+    }
+    
+    func regionsChanged() {
+        
+        countriesInEnabledRegions.removeAll()
+        
+        for filename in allCountries {
+            let region = filename.componentsSeparatedByString("-")[0]
+        
+            if (enabledRegions[region]!) {
+                countriesInEnabledRegions.append(filename)
+            }
+        }
+    }
+    
+    var regions: [String : Bool] {
+        return enabledRegions
+    }
+    
+    var enabledRegionCountries: [String] {
+        return countriesInEnabledRegions
+    }
+    
+    func toggleRegion(name: String) {
+        enabledRegions[name] = !(enabledRegions[name]!)
+        NSUserDefaults.standardUserDefaults().setObject(enabledRegions as NSDictionary, forKey: regionsKey)
+        NSUserDefaults.standardUserDefaults().synchronize()
+        regionsChanged()
+    }
+    
+    func setNumberOfGuesses(guesses: Int) {
+        numberOfGuesses = guesses
+        NSUserDefaults.standardUserDefaults().setInteger(numberOfGuesses, forKey: guessesKey)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    func notifyDelegate() {
+        delegate.settingsChanged()
+    }
+    
+    func newQuizCountries() -> [String] {
+        var quizCountries: [String] = []
+        
+        var flagCounter = 0
+        
+        // add 10 random filenames (countries) to quizCountries
+        while flagCounter < numberOfGuesses {
+            let randomIndex = Int(arc4random_uniform(UInt32(enabledRegionCountries.count)))
+            let filename = enabledRegionCountries[randomIndex]
+            
+            // if image's filename is not in quizCountries, add it
+            if quizCountries.filter({$0 == filename}).count == 0 {
+                quizCountries.append(filename)
+                ++flagCounter
+            }
+        }
+        return quizCountries
+    }
+}
